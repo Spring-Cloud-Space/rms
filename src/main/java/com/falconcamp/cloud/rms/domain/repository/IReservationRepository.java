9d28178671d6e13a0c44d9c7ebb09a85dfdfdd83
yul <yu.li@tecsys.com>
2020-08-27 11:48:44 -0400

    Add springdoc support for REST API. Closes #23

--- src/main/java/com/falconcamp/cloud/rms/domain/repository/IReservationRepository.java
+++ src/main/java/com/falconcamp/cloud/rms/domain/repository/IReservationRepository.java
@@ -17,6 +17,32 @@ import java.util.UUID;
 
 public interface IReservationRepository extends JpaRepository<Reservation, UUID> {
 
+    /*
+     * LockModeType.OPTIMISTIC_FORCE_INCREMENT
+     *   - 	Always increases the entity version
+     *      (even when the entity doesn’t change) and issues a version check
+     *      upon transaction commit, therefore ensuring optimistic locking
+     *      repeatable reads.
+     *   - Same as LockModeType.WRITE
+     *
+     * LockModeType.OPTIMISTIC
+     *   - Always issues a version check upon transaction commit,
+     *     therefore ensuring optimistic locking repeatable reads.
+     *   - Same as LockModeType.READ
+     *
+     * LockModeType.PESSIMISTIC_READ
+     *   - A shared lock is acquired to prevent any other transaction from
+     *     acquiring a PESSIMISTIC_WRITE lock
+     *
+     * LockModeType.PESSIMISTIC_WRITE
+     *   - An exclusive lock is acquired to prevent any other transaction from
+     *     acquiring a PESSIMISTIC_READ or a PESSIMISTIC_WRITE lock
+     *
+     * LockModeType.PESSIMISTIC_FORCE_INCREMENT
+     *   - A database lock is acquired to prevent any other transaction from
+     *     acquiring a PESSIMISTIC_READ or a PESSIMISTIC_WRITE lock and the
+     *     entity version is incremented upon transaction commit
+     */
     @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
     Optional<Reservation> findById(UUID id);
 
